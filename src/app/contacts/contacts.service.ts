c5471be3772e6d945f6c7338649c6ec87eb28f01
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-13 20:23:42 +0800

    Create Contacts service

---
+++ src/app/contacts/contacts.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Contact } from './contact.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+  private apiUrl = 'http://localhost:3000/contacts';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getContacts(): Observable<Contact[]> {
+    return this.httpClient.get<Contact[]>(this.apiUrl);
+  }
+
+  getContactById(id: number): Observable<Contact> {
+    return this.httpClient.get<Contact>(`${this.apiUrl}/${id}`);
+  }
+
+  addContact(contact: Contact): Observable<Contact> {
+    return this.httpClient.post<Contact>(this.apiUrl, contact);
+  }
+
+  updateContact(contact: Contact): Observable<Contact> {
+    return this.httpClient.put<Contact>(`${this.apiUrl}/${contact.id}`, contact);
+  }
+
+  deleteContact(id: number): Observable<Contact> {
+    return this.httpClient.delete<Contact>(`${this.apiUrl}/${id}`);
+  }
+}
