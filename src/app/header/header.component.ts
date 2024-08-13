4910d9c894c22bd2280d33ee4dba10dadbc73b53
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-13 22:06:29 +0800

    Update header component and add openModal method

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  @Output() openNewContactModal = new EventEmitter<void>();
 
+  openModal(): void {
+    this.openNewContactModal.emit();
+  }
 }
