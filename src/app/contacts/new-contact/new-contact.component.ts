57b6d0cb559bc1539d1178785b29df17efa137e7
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-13 21:44:50 +0800

    Generate NewContact component

---
+++ src/app/contacts/new-contact/new-contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-contact.component.html',
+  styleUrl: './new-contact.component.css'
+})
+export class NewContactComponent {
+
+}
