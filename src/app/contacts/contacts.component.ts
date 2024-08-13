db38c4e701eeb4187e985d27aa32c89850aa1c3a
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-13 23:28:04 +0800

    Implement fetch contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,29 +1,44 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { ContactsService } from './contacts.service';
 import { GridViewComponent } from './grid-view/grid-view.component';
 import { ListViewComponent } from './list-view/list-view.component';
+import { NewContactComponent } from './new-contact/new-contact.component';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NewContactComponent } from "./new-contact/new-contact.component";
-import { HeaderComponent } from "../header/header.component";
+import { Contact } from './contact.model';
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [CommonModule, GridViewComponent, ListViewComponent, HttpClientModule, NewContactComponent, HeaderComponent],
+  imports: [CommonModule, GridViewComponent, ListViewComponent, NewContactComponent, HttpClientModule],
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.css'
+  styleUrls: ['./contacts.component.css']
 })
-export class ContactsComponent {
-  @Input() isModalOpen = false;
-  @Output() closeModal = new EventEmitter<void>();
+export class ContactsComponent implements OnInit {
+  @Input() isModalOpen = false; // Add @Input() to allow binding
+  @Output() closeModal = new EventEmitter<void>(); // Add @Output() for the close event
+
   currentView: 'grid' | 'list' = 'grid';
+  contacts: Contact[] = [];
+
+  constructor(private contactsService: ContactsService) {}
+
+  ngOnInit(): void {
+    this.fetchContacts();
+  }
 
   setView(view: 'grid' | 'list'): void {
     this.currentView = view;
   }
 
-  closeModalAndReset(): void {
+  fetchContacts(): void {
+    this.contactsService.getContacts().subscribe((data) => {
+      this.contacts = data;
+    });
+  }
+
+  closeModalAndRefresh(): void {
     this.closeModal.emit();
+    this.fetchContacts(); 
   }
 }
