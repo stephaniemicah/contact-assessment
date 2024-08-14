efb0501354658f62ad6090bde406c51743f998cf
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-15 01:17:45 +0800

    Implement routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ViewContactComponent } from './contacts/view-contact/view-contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: ContactsComponent
+  },
+  {
+    path: 'view-contact/:id',
+    component: ViewContactComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
