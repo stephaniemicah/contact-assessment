553a46179cfc2718b68751a80954fe7464b570ec
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-14 22:48:27 +0800

    Add toast

--- src/app/contacts/contacts.service.ts
+++ src/app/contacts/contacts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Contact } from './contact.model';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,7 @@ export class ContactsService {
     return this.httpClient.get<Contact[]>(this.apiUrl);
   }
 
-  getContactById(id: string): Observable<Contact> { // Changed to string
+  getContactById(id: string): Observable<Contact> {
     return this.httpClient.get<Contact>(`${this.apiUrl}/${id}`);
   }
 
@@ -27,7 +28,25 @@ export class ContactsService {
     return this.httpClient.put<Contact>(`${this.apiUrl}/${contact.id}`, contact);
   }
 
-  deleteContact(id: string): Observable<Contact> { // Changed to string
+  deleteContact(id: string): Observable<Contact> { 
     return this.httpClient.delete<Contact>(`${this.apiUrl}/${id}`);
+  }
+
+  sweetAlert(){
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "bottom-end",
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+    Toast.fire({
+      icon: "success",
+      title: "Contact successfully added"
+    });
   }
 }
