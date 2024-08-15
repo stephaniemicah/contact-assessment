d71af7f43eda56ea2b4ae103c051c719516a0dfe
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-15 09:26:39 +0800

    Add not found route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ViewContactComponent } from './contacts/view-contact/view-contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     path: '',
     redirectTo: '/home',
     pathMatch: 'full'
+  },
+  {
+    path: '**', 
+    component: PageNotFoundComponent
   }
 ];
